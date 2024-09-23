f8e7ff057211d4587c0cdcaeb01278f07a633593
Somk0 <stepanm408@gmail.com>
2024-09-23 12:01:45 +0300

    додав похибку

--- Lab_2/tab_func1.py
+++ Lab_2/tab_func1.py
@@ -6,6 +6,8 @@ ls = np.arange(7.5, 10, 0.2)
 
 i = 1
 x = 0
+range_1 = 8
+range_2 = 9
 
 
 for x in ls:
@@ -13,13 +15,13 @@ for x in ls:
     func_2 = 1 / (sin(x) + fabs(cos(x)))
     func_3 = log((e**x) + 4)
 
-    if x <= 8 :
+    if x <= range_1 :
         print("1. "f"{i} : {func_1}")
         i = i + 1
-    elif 8 < x and x <= 9 :
+    elif range_1 < x and x <= range_2 :
         print("2. "f"{i} : {func_2}")
         i = i + 1
-    elif x > 9 :
+    elif x > range_2 :
         print("3. "f"{i} : {func_3}")
         i = i + 1
 
--- Lab_2/tab_func2.py
+++ Lab_2/tab_func2.py
@@ -1,20 +1,23 @@
-from math import factorial as fak
+from math import factorial as fac
+from math import fabs
 import numpy as np
 
 m = 3
 n = 1
 x = 0
+d = 0.001 
 
 ls = np.arange(0.1, 0.5, 0.05)
 sigma = []
 
 for x in ls :
 
-    func = ((m *(m + 1)*(m + n - 1))/(fak(n)))*x**n
-    
-    if x <= ls[-1] :
+    func = ((m *(m + 1)*(m + n - 1))/(fac(n)))*x**n
+
+    if abs(func) >= d :
         sigma.append(func)
-        n += 1  
+        n += 1 
 
 
 print(1 + sum(sigma))
+print(fabs(-1))
