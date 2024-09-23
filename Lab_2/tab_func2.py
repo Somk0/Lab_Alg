2abbb569dfedea01934402127bd74a09af1bd062
Somk0 <stepanm408@gmail.com>
2024-09-23 20:46:50 +0300

    переписав arange

--- Lab_2/tab_func2.py
+++ Lab_2/tab_func2.py
@@ -1,23 +1,38 @@
 from math import factorial as fac
-from math import fabs
-import numpy as np
 
+def arange(a ,b ,c):
+    r = []
+
+    r.append(a)
+    while True:
+
+        if a >= b:
+            break
+
+        a = a + c
+
+        r.append(a)
+        
+    return r 
 m = 3
 n = 1
 x = 0
 d = 0.001 
 
-ls = np.arange(0.1, 0.5, 0.05)
+ls = arange(0.1, 0.5, 0.05)
+
 sigma = []
 
 for x in ls :
 
     func = ((m *(m + 1)*(m + n - 1))/(fac(n)))*x**n
 
-    if abs(func) >= d :
+    if func >= d :
+        
         sigma.append(func)
-        n += 1 
 
+    n += 1 
 
 print(1 + sum(sigma))
-print(fabs(-1))
+
+
